98d151421307ef90f61faa2e4ba61eaf2f96c346
Kaulick Patra <138438959+Kaulick@users.noreply.github.com>
2024-07-28 13:11:48 +0530

    Add files via upload

---
+++ Digital Clock.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+import time
+
+def time_update():
+    current_time = time.strftime('%I:%M:%S %p')
+    time_part, am_pm_part = current_time[:-3], current_time[-2:]
+    clock_label.config(text=time_part)
+    am_pm_label.config(text=am_pm_part)
+    clock_label.after(1000, time_update)
+
+root = tk.Tk()
+root.title("Digital Clock")
+
+# Make the window full screen
+root.attributes('-fullscreen', True)
+root.configure(background='black')
+
+# Exit full screen on pressing the Escape key
+root.bind("<Escape>", lambda event: root.attributes("-fullscreen", False))
+
+# Create a frame to hold both time and AM/PM labels
+frame = tk.Frame(root, background='black')
+frame.pack(expand=True)
+
+clock_label = tk.Label(frame, font=('DS-Digital', 250, ''), background='black', foreground='red')
+clock_label.pack(side='left')
+
+am_pm_label = tk.Label(frame, font=('DS-Digital', 50, 'bold'), background='black', foreground='red')
+am_pm_label.pack(side='left', padx= 10)
+
+time_update()
+
+root.mainloop()
